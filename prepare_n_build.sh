eeeef83b6f1ef042508f832d7c51d79033fa0eb4
bbaranoff <37385191+bbaranoff@users.noreply.github.com>
2019-11-20 23:06:31 +0100

    Add files via upload

--- prepare_n_build.sh
+++ prepare_n_build.sh
@@ -5,12 +5,12 @@ git clone https://github.com/bbaranoff/ss7MAPer
 cd ss7MAPer
 rebar get-deps
 
-cd deps/osmo_map   
-patch -p1 < ../../patches/osmo_map.patch   
-cd ../osmo_sccp   
-patch -p1 < ../../patches/osmo_sccp.patch   
-cd ../osmo_ss7   
-patch -p1 < ../../patches/osmo_ss7.patch 
+cd deps/osmo_map
+patch -p1 < ../../patches/osmo_map.patch
+cd ../osmo_sccp
+patch -p1 < ../../patches/osmo_sccp.patch
+cd ../osmo_ss7
+patch -p1 < ../../patches/osmo_ss7.patch
 
 cd ../../
 sed -i 's/DRV_CFLAGS/CFLAGS/g' deps/epcap/rebar.config
@@ -27,19 +27,19 @@ cp deps/signerl/TCAP/asn_src/ITU/*rl deps/osmo_sccp/src/
 
 mkdir deps/epcap/deps
 ln -sd ../../pkt deps/epcap/deps/pkt
-mkdir deps/osmo_sccp/deps   
-ln -sd ../../osmo_ss7 deps/osmo_sccp/deps/osmo_ss7   
-ln -sd ../../epcap deps/osmo_sccp/deps/epcap   
-ln -sd ../../pkt deps/osmo_sccp/deps/pkt   
-ln -sd ../../signerl/MAP deps/osmo_sccp/deps/MAP    
-ln -sd ../../signerl/SCCP deps/osmo_sccp/deps/SCCP   
-ln -sd ../../signerl/TCAP deps/osmo_sccp/deps/TCAP   
-mkdir deps/osmo_map/deps   
-ln -sd ../../osmo_ss7 deps/osmo_map/deps/osmo_ss7   
-ln -sd ../../epcap deps/osmo_map/deps/epcap   
+mkdir deps/osmo_sccp/deps
+ln -sd ../../osmo_ss7 deps/osmo_sccp/deps/osmo_ss7
+ln -sd ../../epcap deps/osmo_sccp/deps/epcap
+ln -sd ../../pkt deps/osmo_sccp/deps/pkt
+ln -sd ../../signerl/MAP deps/osmo_sccp/deps/MAP
+ln -sd ../../signerl/SCCP deps/osmo_sccp/deps/SCCP
+ln -sd ../../signerl/TCAP deps/osmo_sccp/deps/TCAP
+mkdir deps/osmo_map/deps
+ln -sd ../../osmo_ss7 deps/osmo_map/deps/osmo_ss7
+ln -sd ../../epcap deps/osmo_map/deps/epcap
 ln -sd ../../pkt deps/osmo_map/deps/pkt
 
-cp deps/signerl/SCCP/itu/include/sccp.hrl deps/osmo_sccp/src/   
+cp deps/signerl/SCCP/itu/include/sccp.hrl deps/osmo_sccp/src/
 cp deps/signerl/TCAP/include/tcap.hrl deps/osmo_map/src/
 
 rebar co
